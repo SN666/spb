d707df680f235380b9e1ac838f2fc1cee6608bc9
SN666 <@setup>
2019-11-21 08:45:36 +0800

    初次提交

---
+++ tspb/src/main/java/com/wdc/tspb/TspbApplication.java
@@ -0,0 +1,13 @@
+package com.wdc.tspb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TspbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TspbApplication.class, args);
+    }
+
+}
---
+++ tspb/src/test/java/com/wdc/tspb/TspbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wdc.tspb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TspbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
